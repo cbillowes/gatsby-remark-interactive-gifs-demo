84abb31a588bbdd2b5c2bc8e0eceec51a888b904
Clarice Bouwer <cbillowes@gmail.com>
2020-04-26 16:17:27 +0400

    Attribute flaticon

--- src/components/layout.js
+++ src/components/layout.js
@@ -40,6 +40,13 @@ const Layout = ({ children }) => {
             <Blog />
           </a>
         </div>
+        <div className="baby">
+          Icons made by{" "}
+          <a href="https://www.flaticon.com/authors/roundicons" title="Roundicons">Roundicons</a>, {" "}
+          <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> and{" "}
+          <a href="https://www.flaticon.com/authors/vitaly-gorbachev" title="Vitaly Gorbachev">Vitaly Gorbachev</a> from{" "}
+          <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>.{" "}
+        </div>
         &copy;
           {new Date().getFullYear()} Clarice Bouwer. Built with
           {` `}
