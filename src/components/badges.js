c6a2a5a90df291bfa6da2b0f11bdfeeea1008088
Clarice Bouwer <cbillowes@gmail.com>
2020-10-06 01:31:26 +0400

    Remove codecov 😞

--- src/components/badges.js
+++ src/components/badges.js
@@ -10,10 +10,8 @@ const Badges = () => (
       rel="noopener noreferrer" >
       <img src="https://travis-ci.org/cbillowes/gatsby-remark-interactive-gifs.svg?branch=master" alt="" />
     </a>
-    <a href="https://codecov.io/gh/cbillowes/gatsby-remark-interactive-gifs"
-      rel="noopener noreferrer" >
-      <img src="https://codecov.io/gh/cbillowes/gatsby-remark-interactive-gifs/branch/master/graph/badge.svg" alt="" />
-    </a>
+    <a href="https://codeclimate.com/github/cbillowes/gatsby-remark-interactive-gifs/maintainability">
+      <img src="https://api.codeclimate.com/v1/badges/c0a664d4b55ae9252de4/maintainability" /></a>
     <a href="https://opensource.org/licenses/MIT"
       rel="noopener noreferrer" >
       <img src="https://img.shields.io/badge/License-MIT-yellow.svg" alt="" />
